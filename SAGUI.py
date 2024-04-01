80e428f97f865ecb98e2b8a94d29275d59e1b1f2
SergaliciousMess <cometkazi25@gmail.com>
2024-04-01 17:53:07 -0400

    added elements to GUI using tkinter

--- SAGUI.py
+++ SAGUI.py
@@ -1,15 +1,27 @@
 import SentimentAnalysis
 import torch
 from torch.utils.data import DataLoader
+import tkinter as tk
+from tkinter import ttk
 
+root = tk.Tk()
+root.geometry('900x700')
+root.title('Sentiment Analysis')
 
-data = SentimentAnalysis.load_dataset('test_data.csv', 'csv')
+title_label = tk.Label(root, text='Sentiment Analysis', font=('Arial', 20))
+title_label.pack(padx=20, pady=10)
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
-dataloader = DataLoader(data, batch_size = 128, shuffle=True, generator=torch.Generator(device=device))
+button_frame = tk.Frame(root)
 
-model = SentimentAnalysis.SentimentAnalysis(dataloader=dataloader, device=device)
+button_frame.columnconfigure(0, weight=1)
+button_frame.columnconfigure(1, weight=1)
 
-print("starting training: " + device)
-model.train_from_dataloader(dataloader=dataloader, epochs=2)
-print(model.analyze(['I love life!','I hate life!','monday, am i right',"weather is looking pretty good today"]))
+create_model_button = tk.Button(button_frame, text="Create new model", font = ('Arial', 16), width=20)
+create_model_button.grid(row=0, column=0, padx=30, sticky=tk.E+tk.W)
+
+load_model_button = tk.Button(button_frame, text="Load model", font = ('Arial', 16), width=20)
+load_model_button.grid(row=0, column=1, padx=30, sticky=tk.E+tk.W)
+
+button_frame.pack(pady=50)
+
+root.mainloop()
