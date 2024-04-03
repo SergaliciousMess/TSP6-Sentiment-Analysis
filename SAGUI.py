230e737e26f93e8ba9aeb3eda6c344db4bafcaba
SergaliciousMess <cometkazi25@gmail.com>
2024-04-03 13:56:36 -0400

    made SAGUI a class

--- SAGUI.py
+++ SAGUI.py
@@ -2,26 +2,32 @@ import SentimentAnalysis
 import torch
 from torch.utils.data import DataLoader
 import tkinter as tk
+from tkinter import messagebox
 from tkinter import ttk
 
-root = tk.Tk()
-root.geometry('900x700')
-root.title('Sentiment Analysis')
 
-title_label = tk.Label(root, text='Sentiment Analysis', font=('Arial', 20))
-title_label.pack(padx=20, pady=10)
+class SAGUI():
+    def __init__(self):
+        root = tk.Tk()
+        root.geometry('900x700')
+        root.title('Sentiment Analysis')
 
-button_frame = tk.Frame(root)
+        title_label = tk.Label(root, text='Sentiment Analysis', font=('Arial', 20))
+        title_label.pack(padx=20, pady=10)
 
-button_frame.columnconfigure(0, weight=1)
-button_frame.columnconfigure(1, weight=1)
+        button_frame = tk.Frame(root)
 
-create_model_button = tk.Button(button_frame, text="Create new model", font = ('Arial', 16), width=20)
-create_model_button.grid(row=0, column=0, padx=30, sticky=tk.E+tk.W)
+        button_frame.columnconfigure(0, weight=1)
+        button_frame.columnconfigure(1, weight=1)
 
-load_model_button = tk.Button(button_frame, text="Load model", font = ('Arial', 16), width=20)
-load_model_button.grid(row=0, column=1, padx=30, sticky=tk.E+tk.W)
+        create_model_button = tk.Button(button_frame, text="Create new model", font = ('Arial', 16), width=20)
+        create_model_button.grid(row=0, column=0, padx=30, sticky=tk.E+tk.W)
 
-button_frame.pack(pady=50)
+        load_model_button = tk.Button(button_frame, text="Load model", font = ('Arial', 16), width=20, command = lambda: messagebox.showerror(title="Error", message="Not implemented"))
+        load_model_button.grid(row=0, column=1, padx=30, sticky=tk.E+tk.W)
 
-root.mainloop()
+        button_frame.pack(pady=50)
+
+        root.mainloop()
+
+SAGUI()
