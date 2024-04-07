949afe62bc2a0401b6c18b401e4515e34e20ea9c
SunOfLife1 <yfitzge@gmail.com>
2024-04-06 21:37:04 -0400

    Move Arg Parsing into parse_args() in cli.py
    - Creates a parse_args() function in cli.py containing the code to parse arguments from the command line
    - Minor formatting adjustments

--- cli.py
+++ cli.py
@@ -8,25 +8,68 @@ Description: _desc_
 import argparse
 from sklearn import metrics, model_selection
 from torch.utils.data import DataLoader
-from SentimentAnalysis import (SentimentAnalysis, ground_truth_label_for_text,
-                               load_dataset)
+from SentimentAnalysis import (Architecture, SentimentAnalysis,
+                               ground_truth_label_for_text, load_dataset)
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Sentiment Analysis CLI')
+    parser.add_argument(
+        '--architecture',
+        type=Architecture,
+        choices=Architecture,
+        default=Architecture.STANDARD,
+        help='Select the CNN architecture (default: standard)'
+    )
+    parser.add_argument(
+        '--learning-rate',
+        type=float,
+        default=0.1,
+        help='Learning rate for training (default: 0.1)'
+    )
+    parser.add_argument(
+        '--batch-size',
+        type=int,
+        default=64,
+        help='Batch size for training (default: 64)'
+    )
+    parser.add_argument(
+        '--epochs',
+        type=int,
+        default=2000,
+        help='Number of epochs for training (default: 2000)'
+    )
+    parser.add_argument(
+        '--dataset',
+        type=str,
+        required=True,
+        help='Path to the dataset file'
+    )
+    parser.add_argument(
+        '--delimiter',
+        type=str,
+        default=',',
+        help='Delimiter used for .txt dataset files (default: ",")'
+    )
+    parser.add_argument(
+        '--positive-threshold',
+        type=float,
+        default=0.7,
+        help='Threshold for classifying as Positive (default: 0.7)'
+    )
+    parser.add_argument(
+        '--negative-threshold',
+        type=float,
+        default=0.3,
+        help='Threshold for classifying as Negative (default: 0.3)'
+    )
+
+    return parser.parse_args()
 
 
 def main():
     # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='Sentiment Analysis CLI')
-    parser.add_argument('--architecture', choices=['standard', '1d', 'deep', 'wide', 'parallel', 'dilated', 'attention'], default='standard', help='Select the CNN architecture (default: standard)')
-    parser.add_argument('--learning-rate', type=float, default=0.1, help='Learning rate for training (default: 0.1)')
-    parser.add_argument('--batch-size', type=int, default=64, help='Batch size for training (default: 64)')
-    parser.add_argument('--epochs', type=int, default=2000, help='Number of epochs for training (default: 2000)')
-    parser.add_argument('--dataset', type=str, required=True, help='Path to the dataset file')
-    parser.add_argument('--positive-threshold', type=float, default=0.7, help='Threshold for classifying as Positive (default: 0.7)')
-    parser.add_argument('--negative-threshold', type=float, default=0.3, help='Threshold for classifying as Negative (default: 0.3)')
-    
-    # Add the '--delimiter' argument. Only needed for .txt dataset files
-    parser.add_argument('--delimiter', type=str, default=',', help='Delimiter used for .txt dataset files (default: ",")')
-
-    args = parser.parse_args()
+    args = parse_args()
 
     # Execute functionality based on command-line arguments
     print(f"Architecture: {args.architecture}")
@@ -48,7 +91,7 @@ def main():
     train_dataloader = DataLoader(train_texts, batch_size=args.batch_size)
 
     # Initialize the model with the inputted arguments
-    model = SentimentAnalysis(dataloader=train_dataloader, model_type=args.architecture, learning_rate=args.learning_rate)
+    model = SentimentAnalysis(train_dataloader, model_type=args.architecture, learning_rate=args.learning_rate)
 
     # Train the model
     model.train_from_dataloader(train_dataloader, epochs=args.epochs)
@@ -73,6 +116,7 @@ def main():
     # print(f"Precision: {precision}")
     # print(f"Recall: {recall}")
     # print(f"F1 Score: {f1}")
+
 
 if __name__ == "__main__":
     main()
